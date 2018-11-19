14af85000049254abddfb8e7531026f8026d9552
slucasm <sergilucasmillan@hotmail.com>
2018-11-19 16:32:21 +0100

    Final commit. Swagger funcionando

--- src/main/java/edu/upc/dsa/services/StationServices.java
+++ src/main/java/edu/upc/dsa/services/StationServices.java
@@ -18,50 +18,28 @@ import java.util.LinkedList;
 import java.util.List;
 
 @Api(value = "/stations", description = "Endpoint to Station Service")
-@Path("/stations")
+@Path("/")
 public class StationServices {
 
-    /*private MyBike mb;
+    private MyBike mb;
 
     public StationServices(){
         this.mb = MyBikeImpl.getInstance();
-        if (mb.size()==0){
-
-            this.mb.addStation("Station1","description:: station1", 10, 3, 3);
-            this.mb.addStation("Station2","description:: station2", 10, 3, 3);
-            this.mb.addUser("user1", "Juan", "Lopex");
+        this.mb.addStation("Station1","description:: station1", 10, 3, 3);
+        this.mb.addStation("Station2","description:: station2", 10, 3, 3);
+        this.mb.addUser("user1", "Juan", "Lopex");
+        try {
+            this.mb.addBike("bike101", "descripton", 25.45, "Station1");
+            this.mb.addBike("bike102", "descripton", 70.3, "Station1");
+            this.mb.addBike("bike103", "descripton", 10.2, "Station1");
+            this.mb.addBike("bike201", "descripton", 1325.45, "Station2");
+            this.mb.addBike("bike202", "descripton", 74430.3, "Station2");
+            this.mb.addBike("bike203", "descripton", 1320.2, "Station2");
+        } catch (StationFullException e) {
+            e.printStackTrace();
+        } catch (StationNotFoundException e) {
+            e.printStackTrace();
         }
-    }
-
-    @GET
-    @ApiOperation(value = "get all stations", notes = "asdasd")
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successful", response = Station.class, responseContainer="List of stations")
-    })
-    @Path("/station")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllStations() {
-
-        List<Station> stations = this.mb.allStations();
-
-        GenericEntity<List<Station>> entity = new GenericEntity<List<Station>>(stations) {};
-        return Response.status(201).entity(entity).build()  ;
-
-    }
-
-    @GET
-    @ApiOperation(value = "get all users", notes = "asdasd")
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successful", response = User.class, responseContainer="List of bikes")
-    })
-    @Path("/users")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllUsers() {
-
-        HashMap<String,User> userMap = this.mb.allUsers();
-
-        GenericEntity<HashMap<String,User>> entity = new GenericEntity<HashMap<String,User>>(userMap) {};
-        return Response.status(201).entity(entity).build()  ;
 
     }
 
@@ -71,53 +49,34 @@ public class StationServices {
             @ApiResponse(code = 201, message = "Successful", response = Bike.class, responseContainer="List of bikes"),
             @ApiResponse(code = 404, message = "User not found")
     })
-    @Path("/{user}")
+    @Path("/getAllBikesFromUser/{userId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllBikesFromUser(@PathParam("user") String user) {
-
+    public Response getAllBikesFromUser(@PathParam("userId") String userId) {
         List<Bike> listaBikes;
-        HashMap<String,User> userMap = this.mb.allUsers();
-        User usuario = userMap.get(user);
-        if (usuario!=null){
-            listaBikes = usuario.getListaBikes();
+        try{
+            listaBikes = this.mb.bikesByUser(userId);
             GenericEntity<List<Bike>> entity = new GenericEntity<List<Bike>>(listaBikes){};
             return Response.status(201).entity(entity).build();
         }
-        else{
+        catch (Exception e){
             return Response.status(404).build();
         }
     }
 
     @GET
-    @ApiOperation(value = "get all bikes from station", notes = "asdasd")
+    @ApiOperation(value = "get first bike from station", notes = "asdasd")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successful", response = Bike.class, responseContainer="List of bikes"),
-            @ApiResponse(code = 404, message = "Station not found")
+            @ApiResponse(code = 201, message = "Successful", response = Bike.class, responseContainer="Bike"),
+            @ApiResponse(code = 404, message = "Station or User not found")
     })
-    @Path("/{station}")
+    @Path("/getBike/{station}/{user}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllBikesFromStation(@PathParam("station") String station) {
-        boolean retornar = false;
-        List<Bike> listaBikes = new LinkedList<>();
-        List<Station> listastations = this.mb.allStations();
-        for (int i = 0; i < listastations.size();i++) {
-            if (station.equals(listastations.get(i).getIdStation())){
-                listaBikes = listastations.get(i).getListaBikes();
-
-                retornar = true;
-
-                break;
-            }
-            else {
-
-            }
-
+    public Response getBike(@PathParam("station") String station,@PathParam("user") String user) {
+        try{
+            Bike bike = this.mb.getBike(station,user);
+             return Response.status(201).entity(bike).build();
         }
-        if (retornar == true){
-            GenericEntity<List<Bike>> entity = new GenericEntity<List<Bike>>(listaBikes){};
-            return Response.status(201).entity(entity).build();
-        }
-        else {
+        catch (Exception e){
             return Response.status(404).build();
         }
     }
@@ -128,32 +87,15 @@ public class StationServices {
             @ApiResponse(code = 201, message = "Successful", response = Bike.class, responseContainer="List of bikes"),
             @ApiResponse(code = 404, message = "Station not found")
     })
-    @Path("/{station}")
+    @Path("/getAllBikesSortedByKms/{station}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllBikesSortedByKms(@PathParam("station") String station) {
-        boolean retornar = false;
-        List<Bike> listaBikes = new LinkedList<>();
-        List<Station> listastations = this.mb.allStations();
-        for (int i = 0; i < listastations.size();i++) {
-            if (station.equals(listastations.get(i).getIdStation())){
-                try {
-                    listaBikes = this.mb.bikesByStationOrderByKms(station);
-                    retornar = true;
-                } catch (StationNotFoundException e) {
-                    e.printStackTrace();
-                    return Response.status(404).build();
-                }
-                break;
-            }
-            else {
-
-            }
-        }
-        if (retornar == true){
-            GenericEntity<List<Bike>> entity = new GenericEntity<List<Bike>>(listaBikes){};
+        try{
+            List<Bike> listaBikes = this.mb.bikesByStationOrderByKms(station);
+            GenericEntity<List<Bike>> entity = new GenericEntity<List<Bike>>(listaBikes) {};
             return Response.status(201).entity(entity).build();
         }
-        else {
+        catch(Exception e){
             return Response.status(404).build();
         }
     }
@@ -164,10 +106,9 @@ public class StationServices {
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Successful", response=Station.class)
     })
-
-    @Path("/addstation")
+    @Path("/addStation/{idStation}/{description}/{max}/{lat}/{lon}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response newStation(String idStation, String description, int max, double lat, double lon) {
+    public Response newStation(@PathParam("idStation") String idStation,@PathParam("description") String description,@PathParam("max") int max,@PathParam("lat") double lat,@PathParam("lon") double lon) {
         Station station = new  Station(idStation,description,max,lat,lon);
         this.mb.addStation(idStation,description,max,lat,lon);
         return Response.status(201).entity(station).build();
@@ -176,28 +117,35 @@ public class StationServices {
     @POST
     @ApiOperation(value = "create a new Bike", notes = "asdasd")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successful", response=Bike.class)
+            @ApiResponse(code = 201, message = "Successful", response=Bike.class),
+            @ApiResponse(code = 404, message = "Station not found or full")
     })
-
-    @Path("/addbike")
+    @Path("/addbike/{idBike}/{description}/{kms}/{idStation}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response newBike(String idBike, String description, double kms, String idStation) {
+    public Response newBike(@PathParam("idBike") String idBike,@PathParam("description") String description,@PathParam("kms") double kms,@PathParam("idStation") String idStation) {
         Bike bike = new  Bike(idBike,description,kms,idStation);
         try {
             this.mb.addBike(idBike,description,kms,idStation);
+            return Response.status(201).entity(bike).build();
         } catch (StationFullException e) {
             e.printStackTrace();
+            return Response.status(404).build();
         } catch (StationNotFoundException e) {
             e.printStackTrace();
+            return Response.status(404).build();
         }
-        return Response.status(201).entity(bike).build();
     }
 
-
-
-
-
-
-*/
+    @POST
+    @ApiOperation(value = "create a new user", notes = "asdasd")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful", response=User.class)
+    })
+    @Path("/adduser/{idUser}/{name}/{surname}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response newUser(@PathParam("idUser") String idUser,@PathParam("name") String name,@PathParam("surname") String surname) {
+        this.mb.addUser(idUser,name,surname);
+        return Response.status(201).build();
+    }
 
 }
